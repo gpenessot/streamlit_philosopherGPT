95d22470af6d5ca91dfb4f1e950d0b2f05f59562
Gaël Penessot <gael.penessot@gmail.com>
2023-08-01 20:39:12 +0200

    optimize resource limits

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,11 @@ qdrant_client = QdrantClient(
     api_key=st.secrets['QDRANT']['qdrant_api_key'],
 )
 
-retrieval_model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+@st.cache(ttl=24*3600, hash_funcs={"MyUnhashableClass": lambda _: None})
+def load_model():
+	  return SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+
+retrieval_model = load_model()
 
 # Initialise session state variables
 if 'generated' not in st.session_state:
